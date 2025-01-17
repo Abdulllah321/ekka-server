29417f80934866b4a47eb18526755fd0733b1fed
Abdulllah321 <abdullahsufyan2007@gmail.com>
2025-01-18 01:28:27 +0500

    commit

--- src/server.ts
+++ src/server.ts
@@ -32,7 +32,7 @@ io.on("connection", (socket) => {
 });
 
 // Use the HTTP server with Socket.IO
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
